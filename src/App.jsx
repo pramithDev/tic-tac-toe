9f54cd3ad73d6a8a39b6186d33e76222ec637df0
pulze <malagodap2@gmail.com>
2024-12-04 09:52:24 +0530

    player active changes added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
+import { useState } from "react"
 import GameBoard from "./components/GameBoard"
 import Player from "./components/Player"
+import Log from "./components/Log";
 
 function App() {
-  
+  const [gameTurns, setGameTurns] = useState([]);
+  const [activePlayer, setActiveplayer] = useState('X');
+
+  function handleSelectSquare() {
+    setActiveplayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns(prevTurns => {
+      const UpdatedTurns = [{ }, ...prevTurns]
+    });
+  }
 
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        <GameBoard/>
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
       </div>
+      <Log/>
     </main>
   )
 }
